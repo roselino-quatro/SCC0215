df9d0fa83ff2596603aff4581eca5818cb825713
gabrielvictorcf <zerozen8@hotmail.com>
2021-05-20 12:17:15 -0300

    add selectWhere function for vehicle data

--- source/main.c
+++ source/main.c
@@ -2,13 +2,23 @@
 #include "line.h"
 
 int main(int argc, char *argv[]){
-	FILE* lcsv = fopen(argv[1],"rb");
+	FILE* vcsv = fopen(argv[1],"rb");
+	VehicleData* vData = readVehicleCsv(vcsv);
+	fclose(vcsv);
+	
+	FILE* bin = fopen("teste","wb");
+	writeVehicleBinary(vData,bin);
+	fclose(bin);
+
+	selectVehicleWhere(vData,"JC314",matchVehiclePrefix);
+
+	FILE* lcsv = fopen(argv[2],"rb");
 	LineData* lData = readLineCsv(lcsv);
 	fclose(lcsv);
 	
-	FILE* bin = fopen("lteste","wb");
-	writeLineBinary(lData,bin);
-	fclose(bin);
+	FILE* bin2 = fopen("lteste","wb");
+	writeLineBinary(lData,bin2);
+	fclose(bin2);
 
 	return 0;
 }
