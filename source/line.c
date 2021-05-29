31fda1dac04683476c3c9b02d290a9c56671b621
gabrielvictorcf <zerozen8@hotmail.com>
2021-05-29 13:32:15 -0300

    rename line's struct field names + all references

--- source/line.c
+++ source/line.c
@@ -1,36 +1,36 @@
 #include "line.h"
 
 // Lê e parsea o CSV do arquivo e armazena em uma struct
-LineData* readLineCsv(FILE* csv){
+LTable* readLineCsv(FILE* csv){
 	if(csv == NULL) {
 		printf("Falha no processamentodo arquivo.\n");
 		return NULL;
 	}
-	LineData* lData = malloc(sizeof(LineData));
+	LTable* lData = malloc(sizeof(LTable));
 
 	// Initialize and read header from csv file
-	lData->header.isStable = true;
-	lData->header.regByteOff = 81;
-	lData->header.regQty = 0;
-	lData->header.regRemovedQty = 0;
+	lData->header->stable = true;
+	lData->header->byteOffset = 81;
+	lData->header->qty = 0;
+	lData->header->rmvQty = 0;
 
-	fscanf(csv,"%[^,],",lData->header.descCode);
-	fscanf(csv,"%[^,],",lData->header.descCard);
-	fscanf(csv,"%[^,],",lData->header.descName);
-	fscanf(csv,"%[^\n]\n",lData->header.descLine);
+	fscanf(csv,"%[^,],",lData->header->code);
+	fscanf(csv,"%[^,],",lData->header->card);
+	fscanf(csv,"%[^,],",lData->header->name);
+	fscanf(csv,"%[^\n]\n",lData->header->line);
 
 	// While there are registers in the csv, allocate and process
 	lData->regQty = 1;
-	lData->registers = malloc(lData->regQty * sizeof(LineReg));
+	lData->registers = malloc(lData->regQty * sizeof(LEntry));
 	int rpos = 0;
 	char code[4];
 	while (fscanf(csv,"%[^,],",code) != EOF) {
 		if (rpos+1 == lData->regQty){
 			lData->regQty *= 2;
-			lData->registers = realloc(lData->registers,lData->regQty * sizeof(LineReg));
+			lData->registers = realloc(lData->registers,lData->regQty * sizeof(LEntry));
 		}
 
-		LineReg* cReg = &lData->registers[rpos++];
+		LEntry* cReg = &lData->registers[rpos++];
 		if (code[0] != '*') {
 			cReg->lineCode = atoi(code);
 			cReg->isPresent = true;
@@ -39,28 +39,28 @@ LineData* readLineCsv(FILE* csv){
 			cReg->isPresent = false;
 		}
 		
-		fscanf(csv,"%c,",&cReg->acceptsCard);
+		fscanf(csv,"%c,",&cReg->card);
 
 		fscanf(csv,"%[^,],",cReg->name);
-		cReg->nameSize = strlen(cReg->name);
+		cReg->nameLen = strlen(cReg->name);
 		if(strncmp(cReg->name,"NULO",4) == 0){
 			cReg->name[0] = '\0';
-			cReg->nameSize = 0;
+			cReg->nameLen = 0;
 		}
 		
 		fscanf(csv,"%[^\n]\n",cReg->color);
-		cReg->colorSize = strlen(cReg->color);
-		cReg->regSize = 23 + cReg->nameSize + cReg->colorSize;
+		cReg->colorLen = strlen(cReg->color);
+		cReg->size = 23 + cReg->nameLen + cReg->colorLen;
 	}
 
 	// Shrink registers array back to appropriate size
 	lData->regQty = rpos;
-	lData->registers = realloc(lData->registers,(lData->regQty) * sizeof(LineReg));
+	lData->registers = realloc(lData->registers,(lData->regQty) * sizeof(LEntry));
 	return lData;
 }
 
-// Destructor que libera memoria alocada de uma struct LineData
-bool freeLineData(LineData* lData) {
+// Destructor que libera memoria alocada de uma struct LTable
+bool freeLineData(LTable* lData) {
 	if (lData == NULL) return false;
 
 	free(lData->registers);
@@ -69,50 +69,101 @@ bool freeLineData(LineData* lData) {
 	return true;
 }
 
-// Transfere os dados de uma LineData para um arquivo binario seguindo as regras passadas nas especificaçẽos
-void writeLineBinary(LineData* lData,FILE* binDest){
+LTable* readLineBinary(FILE* bin) {
+	if(bin == NULL) return NULL;	// Caso arquivo passado seja invalido
+
+	LTable* lData = malloc(sizeof(LTable));
+
+	// Read header data - mark the file as unstable until end of read
+	char header[82];
+	fread(&header,sizeof(char),82,bin);
+
+	lData->header->stable = false;
+	memcpy(&lData->header->byteOffset,&header[1],8);
+	memcpy(&lData->header->qty,&header[9],4);
+	memcpy(&lData->header->rmvQty,&header[13],4);
+	memcpy(&lData->header->code,&header[17],15);
+	memcpy(&lData->header->card,&header[32],13);
+	memcpy(&lData->header->name,&header[45],13);
+	memcpy(&lData->header->line,&header[58],24);
+
+	// Iterate through registers and write their data
+	lData->regQty = 1;
+	lData->registers = malloc(lData->regQty * sizeof(LEntry));
+	int rpos = 0;
+	char regBuffer[14];
+	while (fread(&regBuffer,sizeof(char),14,bin) != 0) {
+		// Exponential reallocation check on every entry
+		if (rpos+1 == lData->regQty){
+			lData->regQty *= 2;
+			lData->registers = realloc(lData->registers,lData->regQty * sizeof(LEntry));
+		}
+
+		LEntry* cReg = &lData->registers[rpos++];
+
+		memcpy(&cReg->isPresent,&regBuffer[0], sizeof(char));
+		memcpy(&cReg->size,&regBuffer[1], sizeof(int));
+		memcpy(&cReg->lineCode,&regBuffer[5], sizeof(int));
+		memcpy(&cReg->card,&regBuffer[9], sizeof(char));
+		memcpy(&cReg->nameLen,&regBuffer[10], sizeof(int));
+
+		fread(&cReg->name,sizeof(char),cReg->nameLen,bin);
+		
+		fread(&cReg->colorLen,sizeof(int),1,bin);
+		fread(&cReg->color,sizeof(char),cReg->colorLen,bin);
+	}
+	
+	// Rewind and mark file as stable
+	lData->header->stable = true;
+	lData->regQty = rpos;
+
+	return lData;
+}
+
+// Transfere os dados de uma LTable para um arquivo binario seguindo as regras passadas nas especificaçẽos
+void writeLineBinary(LTable* lData,FILE* binDest){
 	// Write header data - mark the file as unstable until end of write
-	lData->header.isStable = false;
-	fwrite(&lData->header.isStable, sizeof(char), 1, binDest);
-	fwrite(&lData->header.regByteOff, sizeof(long), 1, binDest);
-	fwrite(&lData->header.regQty, sizeof(int), 1, binDest);
-	fwrite(&lData->header.regRemovedQty, sizeof(int), 1, binDest);
-	fwrite(&lData->header.descCode, sizeof(char), 15, binDest);
-	fwrite(&lData->header.descCard, sizeof(char), 13, binDest);
-	fwrite(&lData->header.descName, sizeof(char), 13, binDest);
-	fwrite(&lData->header.descLine, sizeof(char), 24, binDest);
+	lData->header->stable = false;
+	fwrite(&lData->header->stable, sizeof(char), 1, binDest);
+	fwrite(&lData->header->byteOffset, sizeof(long), 1, binDest);
+	fwrite(&lData->header->qty, sizeof(int), 1, binDest);
+	fwrite(&lData->header->rmvQty, sizeof(int), 1, binDest);
+	fwrite(&lData->header->code, sizeof(char), 15, binDest);
+	fwrite(&lData->header->card, sizeof(char), 13, binDest);
+	fwrite(&lData->header->name, sizeof(char), 13, binDest);
+	fwrite(&lData->header->line, sizeof(char), 24, binDest);
 
 	// Iterate through registers and write their data
 	int regpos = 0;
 	while (regpos < lData->regQty){
-		LineReg* curReg = &lData->registers[regpos++];
+		LEntry* curReg = &lData->registers[regpos++];
 		fwrite(&curReg->isPresent, sizeof(char), 1, binDest);
-		fwrite(&curReg->regSize, sizeof(int), 1, binDest);
+		fwrite(&curReg->size, sizeof(int), 1, binDest);
 		fwrite(&curReg->lineCode, sizeof(int), 1, binDest);
-		fwrite(&curReg->acceptsCard, sizeof(char), 1, binDest);
-		fwrite(&curReg->nameSize, sizeof(int), 1, binDest);
-		fwrite(&curReg->name, sizeof(char), curReg->nameSize, binDest);
-		fwrite(&curReg->colorSize, sizeof(int), 1, binDest);
-		fwrite(&curReg->color, sizeof(char), curReg->colorSize, binDest);
+		fwrite(&curReg->card, sizeof(char), 1, binDest);
+		fwrite(&curReg->nameLen, sizeof(int), 1, binDest);
+		fwrite(&curReg->name, sizeof(char), curReg->nameLen, binDest);
+		fwrite(&curReg->colorLen, sizeof(int), 1, binDest);
+		fwrite(&curReg->color, sizeof(char), curReg->colorLen, binDest);
 	}
 
 	// Rewind and mark file as stable
 	rewind(binDest);
-	lData->header.isStable = true;
-	fwrite(&lData->header.isStable,1,1,binDest);
+	lData->header->stable = true;
+	fwrite(&lData->header->stable,1,1,binDest);
 }
 
-void displayLine(LineReg* lReg){
+void displayLine(LEntry* lReg){
 	if(!lReg) return;
 
-	printf("Nome da linha: %s\n",(lReg->nameSize != 0)? lReg->name : "campo com valor nulo");
+	printf("Nome da linha: %s\n",(lReg->nameLen != 0)? lReg->name : "campo com valor nulo");
 	printf("Cor que descreve a linha: %s\n",lReg->color);
 
-	if(lReg->acceptsCard == 'S'){
+	if(lReg->card == 'S'){
 		printf("Aceita cartao: PAGAMENTO  SOMENTE  COM  CARTAO  SEM  PRESENCA  DE COBRADOR\n\n");
-	} else if(lReg->acceptsCard == 'N'){
+	} else if(lReg->card == 'N'){
 		printf("Aceita cartao: PAGAMENTO EM CARTAO E DINHEIRO\n\n");
-	} else if(lReg->acceptsCard == 'F'){
+	} else if(lReg->card == 'F'){
 		printf("Aceita cartao: PAGAMENTO EM CARTAO SOMENTE NO FINAL DE SEMANA\n\n");
 	} else{
 		printf("Aceita cartao: campo com valor nulo\n\n");
@@ -120,26 +171,26 @@ void displayLine(LineReg* lReg){
 };
 
 // Familia de funções para a selectLineWhere
-bool matchLineCode(LineReg* lReg,void* code) {
+bool matchLineCode(LEntry* lReg,void* code) {
 	return (lReg->lineCode == *(int*)code)? true : false;
 };
 
-bool matchLineAcceptCard(LineReg* lReg,void* cardStatus) {
-	return (lReg->acceptsCard == *(char*)cardStatus)? true : false;
+bool matchLineAcceptCard(LEntry* lReg,void* cardStatus) {
+	return (lReg->card == *(char*)cardStatus)? true : false;
 };
 
-bool matchLineName(LineReg* lReg,void* name) {
+bool matchLineName(LEntry* lReg,void* name) {
 	if (lReg->name[0] == '\0') return false;
 	return (strcmp(lReg->name,(char*)name) == 0)? true : false;
 };
 
-bool matchLineColor(LineReg* lReg,void* color) {
+bool matchLineColor(LEntry* lReg,void* color) {
 	if (lReg->color[0] == '\0') return false;
 	return (strcmp(lReg->color,(char*)color) == 0)? true : false;
 };
 
 // Imprime os matchs de uma comparação dentro de uma struct Dat
-void selectLineWhere(LineData* lData,void* key,bool (*match)(LineReg*,void*)) {
+void selectLineWhere(LTable* lData,void* key,bool (*match)(LEntry*,void*)) {
 	if(lData == NULL) {
 		printf("Falha no processamento do arquivo.\n");
 		return;
@@ -148,7 +199,7 @@ void selectLineWhere(LineData* lData,void* key,bool (*match)(LineReg*,void*)) {
 	bool anyMatched = false;
 	int regPos = 0;
 	while(regPos < lData->regQty) {
-		LineReg* curReg = &lData->registers[regPos++];
+		LEntry* curReg = &lData->registers[regPos++];
 		if (!curReg->isPresent) continue;
 
 		if (match(curReg,key)) {
@@ -161,7 +212,7 @@ void selectLineWhere(LineData* lData,void* key,bool (*match)(LineReg*,void*)) {
 }
 
 // Imprime todos os registros não removidos de uma struct
-void selectLine(LineData* lData) {
+void selectLine(LTable* lData) {
 	if(lData == NULL) {
 		printf("Falha no processamento do arquivo.\n");
 		return;
@@ -170,7 +221,7 @@ void selectLine(LineData* lData) {
 	bool anyMatched = false;
 	int regPos = 0;
 	while(regPos < lData->regQty) {
-		LineReg* curReg = &lData->registers[regPos++];
+		LEntry* curReg = &lData->registers[regPos++];
 		if (curReg->isPresent) {
 			displayLine(curReg);
 			anyMatched = true;
--- source/line.h
+++ source/line.h
@@ -6,33 +6,33 @@
 #include <string.h>
 #include <stdbool.h>
 
-typedef struct LINE_HEADER {
-	bool isStable;
-	long regByteOff;
-	int regQty;
-	int regRemovedQty;
-	char descCode[15];
-	char descCard[13];
-	char descName[13];
-	char descLine[24];
-}LineHeader;
+typedef struct _LineInfo{
+	bool stable;
+	long byteOffset;
+	int qty;
+	int rmvQty;
+	char code[15];
+	char card[13];
+	char name[13];
+	char line[24];
+}LInfo;
 
-typedef struct LINE_REGISTER {
+typedef struct _LineHeader{
 	bool isPresent;
-	int regSize;
+	int size;
 	int lineCode;
-	char acceptsCard;
-	int nameSize;
+	char card;
+	int nameLen;
 	char name[100];
-	int colorSize;
+	int colorLen;
 	char color[100];
-}LineReg;
+}LEntry;
 
-typedef struct LINE_DATA {
-	LineHeader header;
+typedef struct _LineTable{
+	LInfo* header;
 	int regQty;
-	LineReg* registers;
-}LineData;
+	LEntry* registers;
+}LTable;
 
 /****
  * Lê e parsea o CSV do arquivo e armazena em uma struct
@@ -40,38 +40,46 @@ typedef struct LINE_DATA {
  * @param csv ponteiro para o arquivo a ser lido
  * @return ponteiro para a struct allocada na função
  */
-LineData* readLineCsv(FILE* csv);
+LTable* readLineCsv(FILE* csv);
 
 /****
- * Destructor que libera memoria alocada de uma struct LineData
+ * Destructor que libera memoria alocada de uma struct LTable
  * 
  * @param lData ponteiro para struct a ser liberada
  * @return boolean para caso a função tenha sucesso ou não
  */
-bool freeLineData(LineData* lData);
-
+bool freeLineData(LTable* lData);
 
 /****
- * Transfere os dados de uma LineData para um arquivo binario seguindo as regras passadas nas especificaçẽos
+ * Transfere os dados de um arquivo binário para uma estrutura LTable
+ * Caso o binário seja invalido retorna NULL
+ * 
+ * @param bin Arquivo binário com os dados
+ * @return LTable* estrutura na memoria com os dados alocados
+ */
+LTable* readLineBinary(FILE* bin);
+
+/****
+ * Transfere os dados de uma LTable para um arquivo binario seguindo as regras passadas nas especificaçẽos
  * 
  * @param lData struct a ser transferida
  * @param binDest arquivo alvo
  */
-void writeLineBinary(LineData* lData,FILE* binDest);
+void writeLineBinary(LTable* lData,FILE* binDest);
 /****
  * Imprime informações do registro de uma linha
  * 
  * @param lReg registro a ser impresso
  */
-void displayLine(LineReg* lReg);
+void displayLine(LEntry* lReg);
 
 /****
- * Destructor que libera memoria alocada de uma struct LineData
+ * Destructor que libera memoria alocada de uma struct LTable
  * 
  * @param lData ponteiro para struct a ser liberada
  * @return boolean para caso a função tenha sucesso ou não
  */
-bool freeLineData(LineData* lData);
+bool freeLineData(LTable* lData);
 
 /****
  * Familia de funções para a selectLineWhere
@@ -79,10 +87,10 @@ bool freeLineData(LineData* lData);
  * @param lReg registro a ser verificado
  * @param secondParameter valor a ser comparado
  */
-bool matchLineCode(LineReg* lReg,void* code);
-bool matchLineAcceptCard(LineReg* lReg,void* cardStatus);
-bool matchLineName(LineReg* lReg,void* name);
-bool matchLineColor(LineReg* lReg,void* color);
+bool matchLineCode(LEntry* lReg,void* code);
+bool matchLineAcceptCard(LEntry* lReg,void* cardStatus);
+bool matchLineName(LEntry* lReg,void* name);
+bool matchLineColor(LEntry* lReg,void* color);
 
 /****
  * Imprime os matchs de uma comparação dentro de uma struct Data
@@ -90,13 +98,13 @@ bool matchLineColor(LineReg* lReg,void* color);
  * @param lData struct a ser buscada
  * @param match ponteiro de função para uma das funções match
  */
-void selectLineWhere(LineData* lData,void* key,bool (*match)(LineReg*,void*));
+void selectLineWhere(LTable* lData,void* key,bool (*match)(LEntry*,void*));
 
 /****
  * Imprime todos os registros não removidos de uma struct
  * 
  * @param lReg registro a ser verificado 
  */
-void selectLine(LineData* lData) {
+void selectLine(LTable* lData);
 
 #endif
