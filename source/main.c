3ab5140950e9b8407f41e0819c0c5ac5dc3d87ea
roselino-quatro <lou.roselino@gmail.com>
2021-05-30 05:51:57 -0300

    added main parsing functions

--- source/main.c
+++ source/main.c
@@ -15,6 +15,9 @@
 
 #define FILE_NAME 1
 #define BIN_FILE_NAME 2
+#define FIELD_NAME 2
+#define INSERT_QNTY 2
+#define FIELD_VALUE 3
 
 // Exemplo de formatar string pra csv
 // char* input = readline(stdin);
@@ -29,10 +32,15 @@ int main(){
 	FILE* csvPointer;
 	FILE* binPointer;
 	VTable* vehiclesTable;
-	LTable* linesTables;
+	LTable* linesTable;
 	int op;
-	do {
+
+	// Loop principal
+	while(!feof(stdin)) {
 		command = readline(stdin);
+
+		if(command == NULL) break;
+
 		op = atoi(command);
 		switch(op) {
 			case CREATE_VEHICLE:
@@ -50,6 +58,7 @@ int main(){
 				
 				binarioNaTela(arguments[BIN_FILE_NAME]);
 				free(arguments[BIN_FILE_NAME]);
+				free(arguments[0]);
 				break;
 			case CREATE_LINE:
 				cleanString(command);
@@ -58,15 +67,15 @@ int main(){
 				free(arguments[FILE_NAME]);
 				binPointer = openFile(arguments[BIN_FILE_NAME], "wb");
 
-				linesTables = readLineCsv(csvPointer);
+				linesTable = readLineCsv(csvPointer);
 				fclose(csvPointer);
-				writeLineBinary(linesTables, binPointer);
+				writeLineBinary(linesTable, binPointer);
 				fclose(binPointer);
-				freeLTable(linesTables);
+				freeLineTable(linesTable);
 				
 				binarioNaTela(arguments[BIN_FILE_NAME]);
 				free(arguments[BIN_FILE_NAME]);
-				free(argumentes[1]);
+				free(arguments[0]);
 				break;
 			case SELECT_VEHICLE:
 				cleanString(command);
@@ -78,217 +87,112 @@ int main(){
 
 				selectVehicle(vehiclesTable);
 
-				freeVehicleTable(vehiclesTable);
+				free(arguments[0]);
+				freeVTable(vehiclesTable);
 				break;
 			case SELECT_LINE:
 				cleanString(command);
 				arguments = getFields(2, command);
 				binPointer = openFile(arguments[FILE_NAME], "rb");
 				free(arguments[FILE_NAME]);
-				linesTables = readLineBinary(binPointer);
+				linesTable = readLineBinary(binPointer);
 				fclose(binPointer);
 
-				selectLine(linesTables);
+				selectLine(linesTable);
 
-				freeLineTable(linesTables);
+				free(arguments[0]);
+				freeLineTable(linesTable);
 				break;
 			case WHERE_VEHICLE:
-				break;
-			case WHERE_LINE:
-				break;
-			case INSERTION_VEHICLE:
-				break;
-			case INSERTION_LINE:
-				break;
-		}
-		
-	} while(!feof(stdin));
+				cleanString(command);
+				arguments = getFields(4, command);
+				binPointer = openFile(arguments[FILE_NAME], "rb");
+				free(arguments[FILE_NAME]);
+				vehiclesTable = readVehicleBinary(binPointer);
+				fclose(binPointer);
 
-
-	char* command = readline(stdin);
-	int op = atoi(command);
-	while(command){
-		char* field = strchr(command,' ')+1;
-		switch(op){
-			case 1:{
-				char* csvName = strndup(field,strcspn(field," "));
-				field = strchr(field,' ')+1;
-				char* binName = strndup(field,strcspn(field,"\0"));
-				FILE* csv = fopen(csvName,"rb");
-				FILE* bin = fopen(binName,"wb");
-				free(csvName);
-
-				VTable* vtable = readVehicleCsv(csv,",");
-				fclose(csv);
-				writeVehicleBinary(vtable,bin);
-				fclose(bin);
-				freeVTable(vtable);
-				
-				binarioNaTela(binName);
-				free(binName);
-				break;
-			}
-			case 2:{
-				char* csvName = strndup(field,strcspn(field," "));
-				field = strchr(command,' ')+1;
-				char* binName = strndup(field,strcspn(field,"\0"));
-				FILE* csv = fopen(csvName,"rb");
-				FILE* bin = fopen(binName,"wb");
-				free(csvName);
-
-				LTable* ltable = readLineCsv(csv);
-				fclose(csv);
-				writeLineBinary(ltable,bin);
-				fclose(bin);
-				
-				binarioNaTela(binName);
-				free(binName);
-				break;
-			}
-			case 3:{
-				char* binName = strndup(field,strcspn(field,"\0"));
-				FILE* bin = fopen(binName,"rb");
-				VTable* vtable = readVehicleBinary(bin);
-				fclose(bin);
-
-				selectVehicle(vtable);
-				
-				binarioNaTela(binName);
-				free(binName);
-				break;
-			}
-			case 4:{
-				char* binName = strndup(field,strcspn(field,"\0"));
-				FILE* bin = fopen(binName,"rb");
-				LTable* ltable = readLineBinary(bin);
-				fclose(bin);
-				
-				selectLine(ltable);
-				
-				// binarioNaTela(binName);
-				free(binName);
-				break;
-			}
-			case 5:{
-				char* binName = strndup(field,strcspn(field,"\0"));
-				field = strchr(command,' ')+1;
-				char* cmpField = strndup(field,strcspn(field," "));
-				field = strchr(command,' ')+1;
-				char* fieldValue = strndup(field,strcspn(field,"\0"));
-
-				void* key = fieldValue;
-				bool (*cmpVehicle)(VEntry*,void*);
-				if(cmpField[0] == 'p'){	// FUTURE-FIX: maybe replace with switch? block will be bigger tho
-					cmpVehicle = matchVehiclePrefix;
-				} else if(cmpField[0] == 'd'){
+				bool (*cmpVehicle)(VEntry*,void*); // Cria um ponteiro de função para ser itoçozada no SelectWhere
+				if(!strcmp(arguments[FIELD_NAME], "prefixo")) {
+				cmpVehicle = matchVehiclePrefix;
+				} else if(!strcmp(arguments[FIELD_NAME], "data")) {
 					cmpVehicle = matchVehicleData;
-				} else if(cmpField[0] == 's'){
+				} else if(!strcmp(arguments[FIELD_NAME], "quantidadeLugares")) {
 					cmpVehicle = matchVehicleSeatQty;
-					int seatQty = atoi(fieldValue);
-					key = &seatQty;
-				} else if(cmpField[0] == 'm'){
+				} else if(!strcmp(arguments[FIELD_NAME], "modelo")) {
 					cmpVehicle = matchVehicleModel;
-				} else {
+				} else if(!strcmp(arguments[FIELD_NAME], "categoria")) {
 					cmpVehicle = matchVehicleCategory;
 				}
 
-				FILE* bin = fopen(binName,"rb");
-				VTable* vtable = readVehicleBinary(bin);
-				fclose(bin);
-
-				selectVehicleWhere(vtable,key,cmpVehicle);
-				freeVTable(vtable);
-				
-				free(binName);
+				selectVehicleWhere(vehiclesTable, arguments[FIELD_VALUE], cmpVehicle);
+				free(arguments[0]);
+				free(arguments[FIELD_NAME]);
+				free(arguments[FIELD_VALUE]);
+				freeVTable(vehiclesTable);
 				break;
-			}
-			case 6:{
-				char* binName = strndup(field,strcspn(field," "));
-				field = strchr(field,' ')+1;
-				char* cmpField = strndup(field,strcspn(field," "));
-				field = strchr(field,' ')+1;
-				char* fieldValue = strndup(field,strcspn(field,"\0"));
-				cleanString(fieldValue);
+			case WHERE_LINE:
+				cleanString(command);
+				arguments = getFields(4, command);
+				binPointer = openFile(arguments[FILE_NAME], "rb");
+				free(arguments[FILE_NAME]);
+				linesTable = readLineBinary(binPointer);
+				fclose(binPointer);
 
-				int code;
-				void* key = fieldValue;
-				bool (*cmpLine)(LEntry*,void*);
-				if(!strncmp(cmpField,"cod",3)){	// FUTURE-FIX: maybe replace with switch? block will be bigger tho
+				bool (*cmpLine)(LEntry*,void*); // Cria um ponteiro de função para ser itoçozada no SelectWhere
+				if(!strcmp(arguments[FIELD_NAME], "codLinha")) {
 					cmpLine = matchLineCode;
-					code = atoi(fieldValue);
-					key = &code;
-				} else if(cmpField[0] == 'a'){
+				} else if(!strcmp(arguments[FIELD_NAME], "aceitaCartao")) {
 					cmpLine = matchLineAcceptCard;
-					key = &fieldValue[0];
-				} else if(cmpField[0] == 'n'){
+				} else if(!strcmp(arguments[FIELD_NAME], "nomeLinha")) {
 					cmpLine = matchLineName;
-					int seatQty = atoi(fieldValue);
-					key = &seatQty;
-				} else if(!strncmp(cmpField,"cor",3)){
+				} else if(!strcmp(arguments[FIELD_NAME], "corLinha")) {
 					cmpLine = matchLineColor;
-				}
+				} 
 
-				FILE* bin = fopen(binName,"rb");
-				LTable* ltable = readLineBinary(bin);
-				fclose(bin);
-
-				// printf("bin name:%s\n",binName);
-				// printf("compare field:%s\n",cmpField);
-				// printf("key:%s\n",(char*)key);
-				selectLineWhere(ltable,key,cmpLine);
-				freeLineTable(ltable);
-				
-				free(binName);
+				selectLineWhere(linesTable, arguments[FIELD_VALUE], cmpLine);
+				free(arguments[0]);
+				free(arguments[FIELD_NAME]);
+				free(arguments[FIELD_VALUE]);
+				freeLineTable(linesTable);
 				break;
-			}
-			case 7:{
-				char* binName = strndup(field,strcspn(field,"\0"));
-				field = strchr(command,' ')+1;
-				char* quantityString = strndup(field,strcspn(field," "));
-				int insertQty = atoi(quantityString);
+			case INSERTION_VEHICLE:
+				cleanString(command);
+				arguments = getFields(3, command);
+				binPointer = openFile(arguments[FILE_NAME], "rb");
+				vehiclesTable = readVehicleBinary(binPointer);
+				fclose(binPointer);
 
-				
-				FILE* bin = fopen(binName,"rb");
-				VTable* vtable = readVehicleBinary(bin);
-				fclose(bin);
+				insertVehicleEntries(vehiclesTable, atoi(arguments[INSERT_QNTY]), ",", binPointer);
+				freeVTable(vehiclesTable);
 
-				insertVehicleEntries(vtable,insertQty,",",bin);
-				freeVTable(vtable);
+				binarioNaTela(arguments[FILE_NAME]);
 
-				binarioNaTela(binName);
-				
-				free(binName);
-			}
-			case 8:{
-				char* binName = strndup(field,strcspn(field," "));
-				field = strchr(field,' ')+1;
-				char* quantityString = strndup(field,strcspn(field,"\0"));
-				int insertQty = atoi(quantityString);
+				free(arguments[0]);
+				free(arguments[FILE_NAME]);
+				free(arguments[INSERT_QNTY]);
 
-				printf("bin name:%s\n",binName);
-				printf("insert:%d\n",insertQty);
-				
-				FILE* bin = fopen(binName,"r+");
-				LTable* ltable = readLineBinary(bin);
+				break;
+			case INSERTION_LINE:
+				cleanString(command);
+				arguments = getFields(3, command);
+				binPointer = openFile(arguments[FILE_NAME], "rb");
+				linesTable = readLineBinary(binPointer);
+				fclose(binPointer);
 
-				insertLineEntries(ltable,insertQty,bin);
-				freeLineTable(ltable);
-				fclose(bin);
+				insertLineEntries(linesTable, atoi(arguments[INSERT_QNTY]), binPointer);
+				freeLineTable(linesTable);
 
-				binarioNaTela(binName);
-				
-				free(binName);
-			}
-			default:
-				printf("O comando veio zuado, nao tava entre [1..9]\n");
-				exit(1);
+				binarioNaTela(arguments[FILE_NAME]);
+
+				free(arguments[0]);
+				free(arguments[FILE_NAME]);
+				free(arguments[INSERT_QNTY]);
+				break;
 		}
-
+		free(arguments);
 		free(command);
-		command = readline(stdin);
+		
 	}
-	
 
-	free(command);
 	return 0;
 }
